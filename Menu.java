5c5d749fe0bb180c22a695695df8cd790e401f02
Kristin Kullberg <kristin.kullberg@gmail.com>
2024-12-10 20:53:00 +0100

    printMenu() egen metod +ersatt switch/return-sats med enklare if/return-sats

--- Menu.java
+++ Menu.java
@@ -4,22 +4,11 @@ public class Menu {
     
     public static int selectGame() {
 
-        System.out.println("Du kan välja på dessa spel");
-        System.out.println("1. Tre i rad mot dator");
-        System.out.println("2. Tre i rad mot en motståndare");
-        System.out.println("3. Fyra i rad mot en motståndare");
-        System.out.println("4. Fem i rad mot en motståndare");
-        System.out.println();
-
         int chooseGame = 0;
-        int currentGameId = 0;
-        boolean choseGameCorrecly = false;
-
-        while (!choseGameCorrecly) {
-
-            System.out.println("Skriv in 1, 2, 3 eller 4 beroende på vilket spel du vill spela");
+        printMenu();
+        while (true) {
+            
             try {
-
                 chooseGame = GlobalTools.intInputFilter(4);
                 System.out.println();
 
@@ -28,34 +17,21 @@ public class Menu {
                 continue;
             }
 
-            switch (chooseGame) {
-                case 1:
-                    choseGameCorrecly = true;
-                    currentGameId = 1; 
-                    return currentGameId;
-
-                case 2:
-                    choseGameCorrecly = true;
-                    currentGameId = 2; 
-                    return currentGameId;
-
-                case 3:
-                    choseGameCorrecly = true;
-                    currentGameId = 3; 
-                    return currentGameId;
-
-                case 4:
-                    choseGameCorrecly = true;
-                    currentGameId = 4; 
-                    return currentGameId;
-
-                default:
-                    System.out.println(
-                            "Spel med siffan " + chooseGame + " finns tyvärr inte i listan. Välj mellan 1,2, 3 eller 4");
-                    break;
+            if(chooseGame >= 1 && chooseGame <=4){
+                return chooseGame;
+            }else{
+                System.out.println("Spel med siffan " + chooseGame + " finns tyvärr inte i listan. Välj mellan 1,2, 3 eller 4");
             }
         }
+    }
 
-        return 0;
+    private static void printMenu(){
+        System.out.println("Du kan välja på dessa spel:");
+        System.out.println("1. Tre i rad mot dator");
+        System.out.println("2. Tre i rad mot en motståndare");
+        System.out.println("3. Fyra i rad mot en motståndare");
+        System.out.println("4. Fem i rad mot en motståndare");
+        System.out.println();
+        System.out.println("Skriv in 1, 2, 3 eller 4 beroende på vilket spel du vill spela.");
     }
 }
